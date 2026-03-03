f7f0d8207ee187409b2372af6429057376ff690d
michellviu <michellviu@gmail.com>
2026-03-02 23:39:03 -0500

    Add IfExpr struct and CondBranch struct with Visitable implementation

---
+++ src/parser/src/ast/expressions/if_expr.rs
@@ -0,0 +1,24 @@
+use crate::ast::{Expression, ExprBody, Visitable, Visitor};
+
+/// `if (cond) body elif (cond2) body2 else body3`
+#[derive(Debug, Clone)]
+pub struct IfExpr {
+    pub condition: Expression,
+    pub then_body: ExprBody,
+    pub elif_branches: Vec<CondBranch>,
+    pub else_body: Option<ExprBody>,
+}
+
+/// A single elif branch: `elif (condition) body`
+#[derive(Debug, Clone)]
+pub struct CondBranch {
+    pub condition: Expression,
+    pub body: ExprBody,
+}
+
+impl Visitable for IfExpr {
+    fn accept<V: Visitor>(&self, visitor: &mut V) {
+        visitor.visit_if_expr(self);
+    }
+}
+
